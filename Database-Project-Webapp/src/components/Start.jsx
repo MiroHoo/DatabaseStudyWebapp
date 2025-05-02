83e9b848162553015eee0c0311dd187f4c71b2ec
miro hannikainen <mirohannikainen@gmail.com>
2025-05-02 15:32:28 +0300

    basic dropdown + start screen assets

--- Database-Project-Webapp/src/components/Start.jsx
+++ Database-Project-Webapp/src/components/Start.jsx
@@ -2,13 +2,31 @@ import '../css/index.css'
 import '../css/start.css'
 import gif from '../assets/Cool.gif'
 function App() {
-  return (
-    <>
-    <div className='StartContainer'>
-    <img src={gif}></img>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className='StartContainer'>
+                <img src={gif}></img>
+                <div className='SelectionContainer'>
+                    <div className='StartList'>
+                      <ListOfAllTest/>
+                    </div>
+                    <button>Start</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
+
+function ListOfAllTest() {
+    var listofTests = [];
+    var siteElement = []; 
+    for(var i = 0; i<3; i++){
+        listofTests[i] = "Test" + i
+    }
+    listofTests.forEach(element => {
+        siteElement.push(<a className='StartListItem'>{element}</a>)
+    });
+    return siteElement
+}
