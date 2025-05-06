188680c482fba2f11524b8612ea9fa20d47e2296
miro hannikainen <mirohannikainen@gmail.com>
2025-05-06 16:09:34 +0300

    CSS changes + a somewhat working burgermenu. Animating and responsivness still needs some work, but all the main functionality should be visible on the main page now.

--- Database-Project-Webapp/src/components/Header.jsx
+++ Database-Project-Webapp/src/components/Header.jsx
@@ -1,18 +1,30 @@
 
+import { useState } from 'react'
 import '../css/index.css'
 
 function App() {
+
+  const [BurgerVis, setBurgerVis] = useState(false)
+
   return (
     <>
       <div className='HeaderContainer'>
         <div className='HeaderContent'>
           <a className='HeaderName'>Database Learning Webapp</a>
-          <a className='Burgermenu' onClick={() => {}}>
+          <a className='Burgermenu' onClick={() => {setBurgerVis(!BurgerVis)}}>
             <div className='burgerlayer'></div>
             <div className='burgerlayer'></div>
             <div className='burgerlayer'></div>
           </a>
         </div>
+        <div>
+          </div>
+      </div>
+      <div className={`BurgerContainer ${BurgerVis ? 'open' : ''}`}>
+      <a className='BurgerOption'>Option 1</a> 
+      <a className='BurgerOption'>Option 2</a>
+      <a className='BurgerOption'>Option 3</a>
+      <a className='BurgerOption'>Option 4</a>
       </div>
     </>
   )
