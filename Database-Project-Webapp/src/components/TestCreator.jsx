d96d3edb4f2c106deb97cca4bfef4a19cdfbb505
miro hannikainen <mirohannikainen@gmail.com>
2025-05-08 20:32:52 +0300

    Working primitive test building ui

--- Database-Project-Webapp/src/components/TestCreator.jsx
+++ Database-Project-Webapp/src/components/TestCreator.jsx
@@ -1,17 +1,43 @@
 
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import '../css/TestCreator.css'
 function App() {
+let questionId = 1
+const [state, setState] = useState([
+  <div className='TestContainer' key={"Question_" + questionId++}>
+  <h className="TestHeader">Question</h>
+  <input></input>
+  <h className="TestHeader">Model Answer</h>
+  <input></input>
+  <button>Verify Model Answer</button>
+  </div>
+  ])
   return (
-    <>
-          <h>Question</h>
-          <input></input>
-          <h>Model Answer</h>
-          <input></input>
-          <button>Verify Model Answer</button>
-          <button>+</button>
+    <>  
+          <div>{state}</div>
+          <button onClick={AddQuestion}>+</button>
     </>
   )
+
+function AddQuestion() {
+  var QuestionArray = [
+    <div className='TestContainer' key={"Question_" + questionId++}>
+    <h className="TestHeader">Question</h>
+    <input></input>
+    <h className="TestHeader">Model Answer</h>
+    <input></input>
+    <button>Verify Model Answer</button>
+    </div>
+    ]
+    setState([
+      ...state,
+      [QuestionArray]
+    ]);
+  
 }
+
+}
+
+
 
 export default App
