d54ffb561d325138527303136ec433d4fd24b198
miro hannikainen <mirohannikainen@gmail.com>
2025-05-08 21:08:15 +0300

    UI css changes

--- Database-Project-Webapp/src/components/TestCreator.jsx
+++ Database-Project-Webapp/src/components/TestCreator.jsx
@@ -3,29 +3,23 @@ import { useState, useEffect } from 'react'
 import '../css/TestCreator.css'
 function App() {
 let questionId = 1
-const [state, setState] = useState([
-  <div className='TestContainer' key={"Question_" + questionId++}>
-  <h className="TestHeader">Question</h>
-  <input></input>
-  <h className="TestHeader">Model Answer</h>
-  <input></input>
-  <button>Verify Model Answer</button>
-  </div>
-  ])
+const [state, setState] = useState([])
+//initialize first question
+useEffect(()=>{AddQuestion()}, [])
   return (
-    <>  
+    <div className='Tests'>  
           <div>{state}</div>
           <button onClick={AddQuestion}>+</button>
-    </>
+    </div>
   )
-
+//function for adding a question to the question array
 function AddQuestion() {
   var QuestionArray = [
     <div className='TestContainer' key={"Question_" + questionId++}>
     <h className="TestHeader">Question</h>
-    <input></input>
+    <input className='TestInput'></input>
     <h className="TestHeader">Model Answer</h>
-    <input></input>
+    <input className='TestInput'></input>
     <button>Verify Model Answer</button>
     </div>
     ]
@@ -33,7 +27,7 @@ function AddQuestion() {
       ...state,
       [QuestionArray]
     ]);
-  
+
 }
 
 }
