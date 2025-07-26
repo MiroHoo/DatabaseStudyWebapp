35cad54a0864ba91094b8e2e6d42fbeffda056a5
miro hannikainen <mirohannikainen@gmail.com>
2025-07-26 04:09:39 +0300

    simple code that has an error right now, mean to make a very simple comparison of answers

--- NodeJS/Routes/TestTaking_route.js
+++ NodeJS/Routes/TestTaking_route.js
@@ -18,21 +18,38 @@ router.post('/com',
 })
 
 router.get('/:id', function(request,response){
+    var correctquery; 
+    var stundetanswer;
+    var teacheranswer;
     TestTaker.getById(request.params.id, function(err,dbResult){
     if (err) {
       response.json(err);
     } else {
-      response.json(dbResult);
+      console.log(dbResult)
+      correctquery = dbResult
     }
     })
-    console.log(dbResult)
-    TestModel.verifyQuestion(dbResults,function(err, dbResult) {
+ console.log("UserQ")
+    TestModel.verifyQuestion(request.body.studentQ,function(err, dbResult) {
     if (err) {
       response.json(err);
     } else {
-      response.json(dbResult);
+      stundetanswer = dbResult
     }
   });
+  console.log("correct")
+  TestModel.verifyQuestion(correctquery,function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      teacheranswer = dbResult
+    }
+  });
+    if(stundetanswer === teacheranswer){
+        response.send('correct')
+    } else {
+        response.send("incorrect")
+    }
 })
 
 
