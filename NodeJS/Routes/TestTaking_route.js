95f5996b44477c4056b3f24d0b567e8440b01716
miro hannikainen <mirohannikainen@gmail.com>
2025-07-28 18:08:46 +0300

    some cases for the comparing so that the queries dont get fired if they aren't needed to

--- NodeJS/Routes/TestTaking_route.js
+++ NodeJS/Routes/TestTaking_route.js
@@ -34,35 +34,43 @@ function asyncgetById(id) {
   });
 }
 
-router.post('/:id', async function(request,response){
-  console.log(request.body)
+router.post('/:id', async function(request,response,cb){
     //Get correct answer
-    console.log("here")
     const correctquery = await asyncgetById(request.params.id);
+    console.log(correctquery[0].Answer)
+    console.log(request.body.studentQ)
+
+    //compare queries to make sure if they are identical
+    if(correctquery[0].Answer === request.body.studentQ){
+       response.json({outcome:true, message:"The answers are the same!"})
+       return 
+    } else if (request.body.studentQ.includes(";") === false){
+      response.json({outcome:false, message:"The answer was incorrect since it was missing the `;`"})
+      return
+    }
     //Run student query
     const studentAnswer = await asyncverifyQuestion(request.body.studentQ);
-    console.log("here")
     //Run teacher query
-    console.log("here")
     const teacherAnswer = await asyncverifyQuestion(correctquery[0].Answer);
     //Run serialize the json and compare
-    console.log("here")
     if(correctquery.includes("ORDER BY") || correctquery.includes("order by")){
       const teacher_arr = JSON.parse(teacherAnswer[0])
       const student_arr = JSON.parse(studentAnswer[0])
       console.log("Teachers array: " + teacher_arr)
       console.log("Students array: " + student_arr)
       if(JSON.stringify(studentAnswer[0]) === JSON.stringify(teacherAnswer[0])){
-        response.json({outcome:true})
+        response.json({outcome:true, message:"The answers were similar!"})
       } else {
         response.json({outcome:false})
       } 
-    } else {
+    } else if (request.body.studentQ.includes("ORDER BY") === false && request.body.studentQ.includes("order by") === false) {
      if(JSON.stringify(studentAnswer[0]) === JSON.stringify(teacherAnswer[0])){
-        response.json({outcome:true})
+        response.json({outcome:true, message:"The answers are the same!"})
     } else {
         response.json({outcome:false})
     } 
+    } else {
+       response.json({outcome:false})
     }
     
 })
