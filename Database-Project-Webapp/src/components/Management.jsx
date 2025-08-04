9956bc16100932666c5adbea44df2758a9806346
miro hannikainen <mirohannikainen@gmail.com>
2025-08-04 07:37:56 +0300

    updating inside the frontend ui

--- Database-Project-Webapp/src/components/Management.jsx
+++ Database-Project-Webapp/src/components/Management.jsx
@@ -10,80 +10,115 @@ function App() {
     const [ShowData, setShow] = useState(false)
     const [loading, setLoading] = useState(true)
     const [open, setItemsOpen] = useState({})
-    const [modal,setModal] = useState(false)
+    const [modal, setModal] = useState(false)
     const [ModalSettings, setSettings] = useState({
-    "type":"",
-    "text":"",
-    "function": "",
+        "type": "",
+        "text": "",
+        "function": "",
     })
     useEffect(() => {
-       fetch('http://127.0.0.1:3002/test/')
-        .then(response => response.json())
-        .then(response => InitOpen(response))
-        .then(response => setLoading(!loading))
-        .catch(error => console.log(error))
-        }, []);
-        return (
-            <>
+        fetch('http://127.0.0.1:3002/test/')
+            .then(response => response.json())
+            .then(response => InitOpen(response))
+            .then(response => setLoading(!loading))
+            .catch(error => console.log(error))
+    }, []);
+    return (
+        <>
             {
-                modal ? <div id="ModalDiv"><ModalSetter/></div> : <></>
+                modal ? <div id="ModalDiv"><ModalSetter /></div> : <></>
             }
-            { loading ? 
-            <></>
-            :
-            <div className='ManagementContainer'> {Testdata.map((c,i)=><><div className={"ManagementHeader"} onClick={()=>setOpen(i)}>{c.Name}</div>{c.Open ? <ShowTests index={i}/>: <></>}</>)} </div>
+            {loading ?
+                <></>
+                :
+                <div className='ManagementContainer'> {Testdata.map((c, i) => <><div className={"ManagementHeader"} onClick={() => setOpen(i)}>{c.Name}</div>{c.Open ? <ShowTests index={i} /> : <></>}</>)} </div>
             }
-            </>
-        )
+        </>
+    )
 
-    function ShowTests(props){  
-        const TestArray = <div key={Testdata[props.index].TestId} className="ManagementItemCont"><div>{Testdata[props.index].TestId}</div><div>List of testresults</div><div>Average score</div><button className="DeleteTest" onClick={()=>{InputModal(Testdata[props.index].Name,Testdata[props.index].TestId,props.index);}}>Delete</button></div>
+    function ShowTests(props) {
+        const TestArray = <div key={Testdata[props.index].TestId} className="ManagementItemCont"><div onClick={() => { InputModal(Testdata[props.index].Name, Testdata[props.index].TestId, props.index) }}>{Testdata[props.index].Name}</div><div>{Testdata[props.index].TestId}</div><div>List of testresults</div><div>Average score</div><button className="DeleteTest" onClick={() => { QuestionModal(Testdata[props.index].Name, Testdata[props.index].TestId, props.index); }}>Delete</button></div>
         return TestArray
     }
-    function InitOpen(res){
-        const arrayofindexes = res.map((c,i)=> {res[i].Open = false; return res[i]})
+    function InitOpen(res) {
+        const arrayofindexes = res.map((c, i) => { res[i].Open = false; return res[i] })
         setTestdata(arrayofindexes)
     }
-    function ModalSetter(){
-        return <Modal Modalsettings={{type:ModalSettings.type, text:ModalSettings.text, function:ModalSettings.function, funcvar:ModalSettings.funcvar}} stateChanger={setModal}/> 
+    function ModalSetter() {
+        return <Modal Modalsettings={{ type: ModalSettings.type, text: ModalSettings.text, function: ModalSettings.function, funcvar: ModalSettings.funcvar }} stateChanger={setModal} />
     }
-    function InputModal(Name,id, index){
-          setSettings({
+    function QuestionModal(Name, id, index) {
+        setSettings({
             type: "question",
-            text:"Delete Question: " + Name + "?",
-            function:DeleteQuestion,
-            funcvar:{"id": id, "i": index}
-            })
-            setModal(!modal);
+            text: "Delete Question: " + Name + "?",
+            function: DeleteQuestion,
+            funcvar: { "id": id, "i": index }
+        })
+        setModal(!modal);
     }
-    function RemoveElement(index){   
-        setTestdata(Testdata.filter((c,i) => i !== index))
+    function InputModal(name, id, index) {
+        setSettings({
+            type: "input",
+            text: "Change " + name + "'s name",
+            function: ChangeName,
+            funcvar: { "id": id, "index": index}
+        })
+        setModal(!modal);
     }
-    function DeleteQuestion(id, funcvar){
-        fetch("http://127.0.0.1:3002/manage/delete/"+funcvar.id).then(response => response.json()).then(response => console.log(response)).then(RemoveElement(funcvar.i))
+
+    function ChangeName(id ,input,funcvar) {
+        console.log(funcvar)
+        const url = "http://127.0.0.1:3002/manage/update/" + funcvar.id
+        console.log(url)
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "name" : input })
+        }
+        fetch(url, options).then(response => response.json()).then(response => console.log(response)).then(UpdateElement(funcvar.name, funcvar.index))
     }
-    function setOpen(index){
-        if(Testdata[index].Open){
-        const updatedarray = Testdata.map((c,i)=>{
-                if(i === index){
-                Testdata[i].Open = false; return Testdata[i]
+
+    function RemoveElement(index) {
+        setTestdata(Testdata.filter((c, i) => i !== index))
+    }
+
+    function UpdateElement(name, index){
+        const array = Testdata.map((c,i)=> {
+            if(i === index){
+                Testdata[i].Name = name; return Testdata[i]
+            } else {
+                return c
+            }
+        })
+    }
+
+    function DeleteQuestion(id, funcvar) {
+        fetch("http://127.0.0.1:3002/manage/delete/" + funcvar.id).then(response => response.json()).then(response => console.log(response)).then(RemoveElement(funcvar.i))
+    }
+    function setOpen(index) {
+        if (Testdata[index].Open) {
+            const updatedarray = Testdata.map((c, i) => {
+                if (i === index) {
+                    Testdata[i].Open = false; return Testdata[i]
                 } else {
                     return c
                 }
             })
-        setTestdata(updatedarray)
+            setTestdata(updatedarray)
         } else {
-        const updatedarray = Testdata.map((c,i)=>{
-                if(i === index){
-                Testdata[i].Open = true; return Testdata[i]
+            const updatedarray = Testdata.map((c, i) => {
+                if (i === index) {
+                    Testdata[i].Open = true; return Testdata[i]
                 } else {
                     return c
                 }
             })
-        setTestdata(updatedarray)
+            setTestdata(updatedarray)
         }
     }
-    
+
 }
 
 export default App
