fb0fc015bc867277d0b9bf336d48e7926a3d8ff6
miro hannikainen <mirohannikainen@gmail.com>
2025-08-15 19:21:47 +0300

    readme updates

--- Database-Project-Webapp/src/components/Arcade.jsx
+++ Database-Project-Webapp/src/components/Arcade.jsx
@@ -136,9 +136,10 @@ const Layout = () => {
         var notVerified = true
         var QuestionID = 0
         while(notVerified === true){
-            while(QuestionID === DbId || QuestionID === 0){     
+            while(QuestionID === DbId || QuestionID === 0){       
                 QuestionID = Math.round(Math.random() * (res[0].MaxId - res[0].MinId) + res[0].MinId)
             }
+           
             setId(QuestionID)
             await fetch(import.meta.env.VITE_url +"/arcade/verifyid/" + QuestionID).then(res => res.json()).then(res => res[0].Question !== undefined ? notVerified=false : notVerified=true)
         }
